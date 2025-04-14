8ef783d446a5a4b318a049b74e0f49570168c20f
Annagnb <a.gonsberg@hbk-bs.de>
2025-04-14 15:48:29 +0200

    testttt

--- template/index.js
+++ template/index.js
@@ -1,55 +1,87 @@
-// Classifier Variable
 let classifier;
-// Model URL
-// HERE
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/6eL2SJyis/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/UGABDL5F4/';
 
-// Video
 let video;
-let flippedVideo;
-// To store the classification
 let label = '';
-
-// Load the model first
+let board = ['', '', '', '', '', '', '', '', '']; 
+let currentIndex = 0; 
 function preload() {
-	classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-	console.log(classifier);
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+    console.log(classifier);
 }
 
 function setup() {
-	createCanvas(320, 260);
-	// Create the video
-	video = createCapture(VIDEO);
-	video.size(320, 240);
-	video.hide();
-
-	// Start classifying
-	classifyVideo();
+    createCanvas(600, 600); 
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+    classifyVideo();
 }
 
 function draw() {
-	background(0);
-	// Draw the video
-	image(video, 0, 0);
+    background(255); 
+    image(video, 0, 0);
 
-	// Draw the label
-	fill(255);
-	textSize(16);
-	textAlign(CENTER);
-	text(label, width / 2, height - 4);
+    fill(0);
+    textSize(16);
+    textAlign(CENTER);
+    text("Erkannt: " + label, width / 2, 270); 
 }
+    drawBoard(); 
 
-// Get a prediction for the current video frame
 function classifyVideo() {
-	classifier.classify(video, gotResult);
+    classifier.classify(video, gotResult); 
 }
 
-// When we get a result
 function gotResult(results) {
-	console.log(results);
-	// The results are in an array ordered by confidence.
-	// console.log(results[0]);
-	label = results[0].label;
-	// Classifiy again!
-	classifyVideo();
+    console.log(results);
+    label = results[0].label; 
 }
+    
+    if ((label === 'X' || label === 'O') && board[currentIndex] === '') {
+        board[currentIndex] = label;
+        currentIndex = (currentIndex + 1) % 9; 
+
+    classifyVideo(); 
+
+function keyPressed() {
+    
+    if (key === 'ArrowRight') {
+        currentIndex = (currentIndex + 1) % 9; 
+    }
+    if (key === 'ArrowLeft') {
+        currentIndex = (currentIndex + 8) % 9; 
+    }
+    if (key === 'ArrowUp') {
+        currentIndex = (currentIndex + 6) % 9; 
+    }
+    if (key === 'ArrowDown') {
+        currentIndex = (currentIndex + 3) % 9; 
+}}
+
+function drawBoard() {
+    let size = 100; 
+    for (let i = 0; i < 9; i++) {
+        let x = (i % 3) * size + 20; 
+        let y = floor(i / 3) * size + 300; 
+        stroke(0); 
+        noFill();
+        rect(x, y, size, size); 
+
+        
+        fill("pink"); 
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text(board[i], x + size / 2, y + size / 2); 
+        
+        if (i === currentIndex) {
+            noFill();
+            stroke(0, 200, 0);
+            strokeWeight(3);
+            rect(x + 2, y + 2, size - 4, size - 4);
+            strokeWeight(1);
+        }
+    }
+}
+
+//bittefunktionier lol
